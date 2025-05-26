6a0ecff8ce883decb276eafb83a2c15f0c9c9ed9
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-05-26 13:56:03 +0200

    ADD: fetch async transformation

--- src/components/useTasks.jsx
+++ src/components/useTasks.jsx
@@ -6,93 +6,103 @@ export default function useTasks() {
 
     const [tasks, setTasks] = useState([]);
 
+    async function getTasks() {
+        try {
+            const res = await fetch(`${url}/tasks`);
+            if (!res.ok) {
+                throw new Error('Errore nel server');
+            }
+            const data = await res.json();
+            setTasks(data);
+            return data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
 
-    function getTasks() {
-        return fetch(`${url}/tasks`)
-            .then(res => {
-                if (!res.ok) throw new Error('Errore nel server');
-                return res.json();
-            })
-            .then(data => {
-                setTasks(data);
+
+    async function addTask(task) {
+        try {
+            const response = await fetch(`${url}/tasks`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(task),
+            });
+
+            if (!response.ok) {
+                throw new Error('Errore nel server');
+            }
+
+            const data = await response.json();
+
+            if (data.success === true) {
+                await getTasks();
                 return data;
-            })
-            .catch(error => {
-                console.error(error);
-                throw error;
-            });
-    }
-
-    function addTask(task) {
-        return fetch(`${url}/tasks`, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(task),
-        })
-            .then(res => {
-                if (!res.ok) throw new Error('Errore nel server');
-                return res.json();
-            })
-            .then(data => {
-                if (data.success === true) {
-                    return getTasks().then(() => data);
-                } else {
-                    throw new Error(data.message);
-                }
-            })
-            .catch(error => {
-                console.error("Errore nell' aggiunta della task :", error);
-                throw error;
-            });
+            } else {
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.error("Errore nell'aggiunta della task:", error);
+            throw error;
+        }
     }
 
 
 
 
-    function removeTask(taskId) {
-        return fetch(`${url}/tasks/${taskId}`, {
-            method: 'DELETE',
-            headers: { 'Content-Type': 'application/json' },
-        })
-            .then(res => {
-                if (!res.ok) throw new Error('Errore nel server');
-                return res.json();
-            })
-            .then(data => {
-                if (data.success === true) {
-                    return getTasks().then(() => data);
-                } else {
-                    throw new Error(data.message);
-                }
-            })
-            .catch(error => {
-                console.error('Errore nella rimozione della task:', error);
-                throw error;
+
+    async function removeTask(taskId) {
+        try {
+            const response = await fetch(`${url}/tasks/${taskId}`, {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
             });
+
+            if (!response.ok) {
+                throw new Error('Errore nel server');
+            }
+
+            const data = await response.json();
+
+            if (data.success === true) {
+                await getTasks();
+                return data;
+            } else {
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.error('Errore nella rimozione della task:', error);
+            throw error;
+        }
     }
 
 
-    function updateTask(taskId, updatedData) {
-        return fetch(`${url}/tasks/${taskId}`, {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(updatedData),
-        })
-            .then(res => {
-                if (!res.ok) throw new Error('Errore nel server');
-                return res.json();
-            })
-            .then(data => {
-                if (data.success === true) {
-                    return getTasks().then(() => data);
-                } else {
-                    throw new Error(data.message);
-                }
-            })
-            .catch(error => {
-                console.error('Errore nella modifica della task:', error);
-                throw error;
+
+    async function updateTask(taskId, updatedData) {
+        try {
+            const response = await fetch(`${url}/tasks/${taskId}`, {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(updatedData),
             });
+
+            if (!response.ok) {
+                throw new Error('Errore nel server');
+            }
+
+            const data = await response.json();
+
+            if (data.success === true) {
+                await getTasks();
+                return data;
+            } else {
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.error('Errore nella modifica della task:', error);
+            throw error;
+        }
     }
 
 
